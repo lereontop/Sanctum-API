8d3a3b6497336a563919e3a64d94462aecf2cad7
Nurudeen <olalerenurudeenakinola@gmail.com>
2024-06-27 23:04:25 +0100

    |

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,18 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreRequest;
+use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function login(){
+    public function login()
+    {
         return 'This login Page';
     }
 
-    public function register(){
-        return 'This register Page';
+    public function register(StoreRequest $storeRequest)
+    {
+        $storeRequest->validated($storeRequest->all());
+        $user = User::create([
+            'name' => $storeRequest->name,
+            'email' => $storeRequest->email,
+            'password' => Hash::make($storeRequest->password)
+
+        ]);
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API token of ' . $user->name)->plainTextToken
+        ]);
+    }
+
+
+
+    public function logout()
+    {
+        return response()->json('This Logout Page');
     }
 }
